f289640437623c14e96655ff2e96fe950b49a685
Neitron <nweris@yandex.ru>
2022-03-31 22:27:56 +0300

    CSS added v0.1

--- src/App.js
+++ src/App.js
@@ -3,12 +3,10 @@ import React, { useEffect } from 'react';
 import Auth from './Pages/Auth/Auth';
 import Home from './Pages/Home/Home';
 import NoMatch from './Pages/NoMatch/NoMatch';
-
 import './App.scss';
 
 function App() {
   const navigate = useNavigate();
-
   // Check Authenticition
   useEffect(() => {
     const token = localStorage.getItem('token');
