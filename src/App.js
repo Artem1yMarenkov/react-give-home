1a43b8cd4423ecfa463c69f1beb717295c4e6417
Mckoev <mckoev@yandex.ru>
2022-04-01 11:46:47 +0300

    made up the page addNew

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import React, { useEffect } from 'react';
 import Auth from './Pages/Auth/Auth';
 import Home from './Pages/Home/Home';
+import AddNew from './Pages/AddNew/AddNew'
 import NoMatch from './Pages/NoMatch/NoMatch';
 
 import './App.scss';
+
+localStorage.setItem('token', '00')
 
 function App() {
   const navigate = useNavigate();
@@ -20,6 +23,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="auth" element={<Auth/>} />
+          <Route path="addnew" element={<AddNew/>} />
           <Route path="*" element={<NoMatch/>} />
         </Routes>
       </section>
--- src/Pages/AddNew/AddNew.jsx
+++ src/Pages/AddNew/AddNew.jsx
@@ -1,5 +1,38 @@
+import { ClassNames } from '@emotion/react';
+import logo from '../../../src/img/logo.svg'
+import './AddNew.css'
+
 export default function AddNew() {
     return (
-        <h1>Это страница добавления объявления</h1>
+        <>
+        <div className='wrapper__header'>
+            <img className='img' src={logo} alt={'logo'}></img>
+            <button className='wrapper__button-quit'>Выйти</button>
+        </div>
+        <div className='wrapper__div'>Добавление записи</div>
+        <div className='wrapper__main'>
+            
+                <div className='wrapper__photo'>
+                    <div className='wrapper__photo_input_text'>Загрузить фото из UI либы</div>
+                </div>
+            
+            <div className='wrapper__description'>
+            <div className='wrapper__div2'>О животном</div>
+            <div className='wrapper__div3'>Название / имя / кличка животного</div>
+            <div className='wrapper__div4'>Введите что-то там</div>
+            <div className='wrapper__div5'>Пару слов туда-сюда сделай</div>
+            <div className='wrapper__div6'>Введите что-то там</div>
+            <div className='wrapper__div7'>Контактная информация</div>
+            <div className='wrapper__div5'>Номер телефона</div>
+            <div className='wrapper__div4'>Введите что-то там</div>
+            <div className='wrapper__div5'>Адрес электронной почты</div>
+            <div className='wrapper__div4'>Введите что-то там</div>
+            <button className='wrapper__button-save'>Сохранить</button>
+            </div>
+        </div>
+        
+        
+       
+        </>
     );
 }
