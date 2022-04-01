ce4a48cd8c7c37b326326372060fc3cd40337155
Mckoev <mckoev@yandex.ru>
2022-04-01 12:58:05 +0300

    change fails

--- src/Pages/AddNew/AddNew.jsx
+++ src/Pages/AddNew/AddNew.jsx
@@ -12,21 +12,21 @@ export default function AddNew() {
         <div className='wrapper__div'>Добавление записи</div>
         <div className='wrapper__main'>
             
-                <div className='wrapper__photo'>
-                    <div className='wrapper__photo_input_text'>Загрузить фото из UI либы</div>
-                </div>
+                
+            <input className='wrapper__photo' placeholder='Загрузить фото из UI либы'></input>
+                
             
             <div className='wrapper__description'>
             <div className='wrapper__div2'>О животном</div>
             <div className='wrapper__div3'>Название / имя / кличка животного</div>
-            <div className='wrapper__div4'>Введите что-то там</div>
+            <input className='wrapper__div4' placeholder='Введите что-то там'></input>
             <div className='wrapper__div5'>Пару слов туда-сюда сделай</div>
-            <div className='wrapper__div6'>Введите что-то там</div>
+            <input className='wrapper__div6' placeholder='Введите что-то там'></input>
             <div className='wrapper__div7'>Контактная информация</div>
             <div className='wrapper__div5'>Номер телефона</div>
-            <div className='wrapper__div4'>Введите что-то там</div>
+            <input className='wrapper__div4' placeholder='Введите что-то там'></input>
             <div className='wrapper__div5'>Адрес электронной почты</div>
-            <div className='wrapper__div4'>Введите что-то там</div>
+            <input className='wrapper__div4' placeholder='Введите что-то там'></input>
             <button className='wrapper__button-save'>Сохранить</button>
             </div>
         </div>
